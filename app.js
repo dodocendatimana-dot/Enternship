509af2c68c9a65bf304020a6f544e7d6a009d93e
ndatimanadot <dodocendatimana@gmail.com>
2026-04-24 15:43:59 +0200

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.listen(port);
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+})
+
+app.get('/', (req, res) => {
+    const blogs = [
+        { title: 'Blog Post 1', snippet: 'This is the first blog post.' },
+        { title: 'Blog Post 2', snippet: 'This is the second blog post.' },
+        { title: 'Blog Post 3', snippet: 'This is the third blog post.' }
+    ];
+    res.render('index', { title: 'home', blogs });
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+app.get('/blog/create', (req, re) => {
+    res.render('about', (title: 'about us'))
+});
+app.use((req, res) => {
+    res.status(404).render('404');
+});
